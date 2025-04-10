c8f02b2c8bf5ab709a5d712c029592058043accf
Valerie Hammer <vkellogg@wisc.edu>
2025-04-10 18:35:50 -0500

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Valerie Hammer on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
